f97800371daa525fec4981ef7841e0210b3f7e4e
Serhii Romanenko <sergromanenko97@gmail.com>
2024-04-23 16:29:57 +0300

    Removed _

--- src/shared/lib/process-key.ts
+++ src/shared/lib/process-key.ts
@@ -1,3 +1,3 @@
 export const processKey = (key: string): string => {
-  return key.split('_')[0].replace(/\//g, '-').toLowerCase().replace(/\s/g, '-');
+  return key.replace(/\//g, '-').toLowerCase().replace(/\s/g, '-');
 }
